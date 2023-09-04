fb7e6b9d7c0da562fee6ffc6855cd3e6e9c7cce8
rianwp <rianwp2015@gmail.com>
2023-09-04 17:07:37 +0700

    adding comments no 21

--- no21.js
+++ no21.js
@@ -1,18 +1,24 @@
+// Buatlah sebuah function untuk melakukan format sorting array di dalam kode Javascript. 
+// Bisa diskusi dengan teman kelompokmu ya!
 const sortAscending = (array) => {
     let swapped;
 
     do {
+        // 1. Set variable swapped ke false sebagai nilai awal
         swapped = false;
-
+        // 2. Melakukan perulangan sebanyak panjang array
         for (let i = 0; i < array.length; i++) {
+            // 3. Mengecek apakah array[i] lebih besar daripada array[i+1]
             if (array[i] > array[i + 1]) {
+                // 4. Melakukan swap nilai array[i] dengan index setelahnya jika nilai array[i] lebih besar dari array[i+1]
                 let temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
+                // 5. Set nilai swapped ke true agar bisa melakukan pengecekan selanjutnya
                 swapped = true;
             }
         }
-    } while (swapped === true);
+    } while (swapped === true); // 6. jika nilai swapped true, maka akan melakukan perulangan lagi hingga nilai array[i] tidak lebih besar dari nilai array[i+1]
     return array;
 }
 
@@ -20,17 +26,21 @@ const sortDescending = (array) => {
     let swapped;
 
     do {
+        // 1. Set variable swapped ke false sebagai nilai awal
         swapped = false;
-
+        // 2. Melakukan perulangan sebanyak panjang array
         for (let i = 0; i < array.length; i++) {
+            // 3. Mengecek apakah array[i] lebih kecil daripada array[i+1]
             if (array[i] < array[i + 1]) {
+                // 4. Melakukan swap nilai array[i] dengan index setelahnya jika nilai array[i] lebih kecil dari array[i+1]
                 let temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
+                // 5. Set nilai swapped ke true agar bisa melakukan pengecekan selanjutnya
                 swapped = true;
             }
         }
-    } while (swapped === true);
+    } while (swapped === true); // 6. jika nilai swapped true, maka akan melakukan perulangan lagi hingga nilai array[i] tidak lebih kecil dari nilai array[i+1]
     return array;
 }
 
