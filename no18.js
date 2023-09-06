ca23a66500c7675f1f5f702b4028c11b32eb73bb
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2023-09-06 16:01:43 +0700

    menambahkan komentar pada no 18

--- no18.js
+++ no18.js
@@ -5,16 +5,25 @@
 
 function solution(str) {
     // tulis jawabanmu disini
+
+    // membuat variabel untuk menampung hasil
     let result = ""
+
+    // melakukan perulangan terhadap setiap karakter str
     for (let i = 0; i < str.length; i++) {
+
+        // jika karakter str pada index i = '.' maka result ditambahkan '()'
         if (str[i] === '.') {
             result += '()';
-        } else {
+        }
+        // jika tidak maka akan ditambahkan biasa
+        else {
             result += str[i];
         }
     }
     return result;
 }
 
+// menampilkan hasil
 console.log(solution("1.1.1.1")) // expected output 1()1()1()1()
 console.log(solution("192.168.1.1")) // expected output 192()268()1()1()
